5b1b498c3ac5bba449531e36405c758be6cd20cb
Agung Prasetyo <prasetyo.agpr@gmail.com>
2024-10-18 14:56:51 +0700

    update social links image build

--- social-links/vite.config.ts
+++ social-links/vite.config.ts
@@ -4,4 +4,26 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+            // Directly saving images to the target images folder
+            return 'images/[name]-[hash][extname]'; // Adjusted path for images
+          }
+
+          if (/\.css$/.test(name ?? '')) {
+            return 'assets/css/[name]-[hash][extname]';
+          }
+
+          // Default value for other assets
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
 })
