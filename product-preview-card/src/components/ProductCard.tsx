c4eb63e9bf85989338e72715ed1786ac7cf57774
Agung Prasetyo <prasetyo.agpr@gmail.com>
2024-11-03 18:32:18 +0700

    update build product-preview-card

--- product-preview-card/src/components/ProductCard.tsx
+++ product-preview-card/src/components/ProductCard.tsx
@@ -5,19 +5,19 @@ import productImageMobile from '../../public/images/image-product-mobile.jpg'; /
 const ProductCard: React.FC = () => {
     return (
         <div className="flex flex-col lg:flex-row max-w-xl lg:mx-auto mx-4 bg-white rounded-lg shadow-lg overflow-hidden">
-            <picture >
-                <source media="(min-width: 1025px)" srcSet={productImageDesktop} className="object-cover h-full w-full bg-red-400"/>
-                <img src={productImageMobile} alt="Gabrielle Essence Eau De Parfum" className="object-cover h-full w-full bg-red-400"/>
-            </picture>
-            <div className="px-6 py-8">
+            <div className="w-full lg:w-1/2"> {/* Set width to 50% on larger screens */}
+                <picture>
+                    <source media="(min-width: 1025px)" srcSet={productImageDesktop} />
+                    <img src={productImageMobile} alt="Gabrielle Essence Eau De Parfum" className="object-cover h-full w-full bg-red-400" />
+                </picture>
+            </div>
+            <div className="px-6 py-8 w-full lg:w-1/2"> {/* Set width to 50% on larger screens */}
                 <h2 className="text-md text-neutral-darkGrayishBlue mb-1">PERFUME</h2>
                 <h1 className="text-4xl font-bold font-fraunces mb-2 text-neutral-veryDarkBlue">
                     Gabrielle
-                    <br className="hidden sm:block"/> {/* Hide on small screens, show on sm and larger */}
+                    <br className="hidden sm:block" /> {/* Hide on small screens, show on sm and larger */}
                     Essence Eau De Parfum
                 </h1>
-
-
                 <p className="text-neutral-darkGrayishBlue mb-6">
                     A floral, solar and voluptuous interpretation composed by Olivier Polge,
                     Perfumer-Creator for the House of CHANEL.
@@ -28,10 +28,9 @@ const ProductCard: React.FC = () => {
                 </div>
                 <button
                     className="flex items-center justify-center w-full min-h-14 bg-primary-darkCyan text-white font-medium py-2 rounded hover:bg-primary-darkerDarkCyan transition">
-                    <img src="/images/icon-cart.svg" alt="Cart Icon" className="mr-2"/>
+                    <img src="/images/icon-cart.svg" alt="Cart Icon" className="mr-2" />
                     Add to Cart
                 </button>
-
             </div>
         </div>
     );
